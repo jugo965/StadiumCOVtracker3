3d9d86942b23d33ebfd418399c8af41637aa7164
jugo965 <113313530+jugo965@users.noreply.github.com>
2022-09-15 18:59:26 +0200

    Add files via upload

---
+++ src/store.js
@@ -0,0 +1,12 @@
+export default {
+    authenticated: false,
+    tribine:'Sjever',
+    stadion:'Maksimir',
+    sektor:'A',
+    userEmail: '',
+    ime:'',
+    telefon:'',
+    prezime:'',
+    
+
+}
